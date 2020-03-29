530b4b90d3820cfeceff83d89cb08386ca3282be
Sourabh Singh Rawat <s.rawat3.142@live.in>
2020-03-30 00:54:00 +0530

    Move fragment creation to code.

--- app/src/main/java/org/nooblabs/simplemusicplayer/MainActivity.kt
+++ app/src/main/java/org/nooblabs/simplemusicplayer/MainActivity.kt
@@ -1,17 +1,29 @@
 package org.nooblabs.simplemusicplayer
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import org.nooblabs.simplemusicplayer.loaders.SongLoader
+import org.nooblabs.simplemusicplayer.ui.SongListFragment
+import org.nooblabs.simplemusicplayer.viewmodels.SongsViewModel
+import org.nooblabs.simplemusicplayer.viewmodels.SongsViewModelFactory
 
 /**
  * Main Activity.
  */
 class MainActivity : AppCompatActivity() {
 
+  private val songsViewModel: SongsViewModel by viewModels { SongsViewModelFactory(SongLoader()) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(findViewById(R.id.main_action_bar))
+
+    supportFragmentManager.beginTransaction().apply {
+      add(R.id.fragment_song_list, SongListFragment(songsViewModel))
+      commit()
+    }
   }
 
   companion object {
